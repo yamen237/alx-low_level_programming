aba0063e1c0d9f74949c9857eb630f7ab90a17d0
yamen237 <joelyameni@gmail.com>
2022-03-15 23:12:35 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,8 @@
  * main-entry point
  * Return: Always 0(Succes)
  */
-int main(void){
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
