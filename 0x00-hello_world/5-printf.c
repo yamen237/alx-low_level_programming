92e4f7542304d6cdfc5d11c8ae82533e2b4e1285
yamen237 <joelyameni@gmail.com>
2022-03-16 00:00:04 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
  /**
  * main-Entry point
+ * my hello world
  * Return: Always 0(Success)
  */
-int main(int argc, char *argv[])
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
