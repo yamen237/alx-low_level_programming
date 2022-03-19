485f9c96f58444e38ee729c376ead8db3929f606
yamen237 <joelyameni@gmail.com>
2022-03-19 05:21:29 -0700

    print alphabet

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,8 +8,8 @@ int main(void)
 char alphabet;
 for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 {
-	if (alphabet != 'e' || alphabet != 'q')
-		 putchar(alphabet);
+if (alphabet != 'e' || alphabet != 'q')
+putchar(alphabet);
 }
 putchar('\n');
 return (0);
