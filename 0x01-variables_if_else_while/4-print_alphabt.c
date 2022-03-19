c2a2302d2350ef04ac83a23d4d218e860e47a834
yamen237 <joelyameni@gmail.com>
2022-03-19 05:06:43 -0700

    #include <stdio.h> /**  * main - Entry point  * Return: Always 0 (Success)  */ int main(void) { char alphabet; for (alphabet = a; alphabet <= z; alphabet++) { if (alphabet != e || alphabet != q)         putchar(alphabe); } putchar(n); return (0); }

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char alphabet;
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+	if (alphabet != 'e' || alphabet != 'q')
+		putchar(alphabe);
+}
+putchar('\n');
+return (0);
+}
