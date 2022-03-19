a1f60d42d618edc57f6793669aef0a651d77eee1
yamen237 <joelyameni@gmail.com>
2022-03-19 15:21:28 -0700

    print comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+int j;
+for ()
+{
+if (i < j)
+{
+putchar(i);
+putchar(j);
+if (i < 56)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
