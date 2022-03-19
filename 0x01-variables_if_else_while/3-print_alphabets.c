89088c3e8e9f91b0f8c3b56b6b72e49c7bf6c812
yamen237 <joelyameni@gmail.com>
2022-03-19 04:41:40 -0700

    print alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,9 +6,16 @@
 int main(void)
 {
 char alphabet;
+
 for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-        putchar(alphabet);
+{
+putchar(alphabet);
+}
+
 for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
-        putchar(alphabet);
+{
+putchar(alphabet);
+}
+
 return (0);
 }
