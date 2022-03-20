18aba29d4ebbfac383a4c86ed2c870a832e74b76
yamen237 <joelyameni@gmail.com>
2022-03-20 00:55:25 -0700

    abs

---
+++ 0x02-functions_nested_loops/abs.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _abs - return the absolut value
+ * @n: take in integer
+ * Return: retunr the absolut number
+ */
+int _abs(int n)
+{
+n = n < 0 ? -1*n : n;
+return (n);
+}
