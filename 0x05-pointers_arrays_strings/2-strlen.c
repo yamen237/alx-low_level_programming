2bc89dd91723c34b58cdc5e597f3ff946a1ad8f8
yamen237 <joelyameni@gmail.com>
2022-03-22 05:23:33 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * int _strlen return the length of a string
+ * @s: take in string to pointeur
+ * Return: return the length of string
+ */
+int _strlen(char *s)
+{
+return strlen(s);
+}
