888b54d4246a3c9a51d4ffcdd7a035ad59af1483
yamen237 <joelyameni@gmail.com>
2022-03-27 08:45:05 -0700

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,9 +8,9 @@
 char *_strcpy(char *dest, char *src)
 {
 int i, len;
-for (len = 0; len < src[len] != '\0'; len++)
+for (len = 0; src[len] != '\0'; len++)
 {}
-for (i = 0; i <= len; i++)
+for (i = 0; i < len; i++)
 {
 dest[i] = src[i];
 }
