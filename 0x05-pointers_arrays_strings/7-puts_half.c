8c1df666cef7df07bf1c680faf05a35b213551b4
yamen237 <joelyameni@gmail.com>
2022-03-27 07:42:54 -0700

    put half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,10 +8,10 @@
 void puts_half(char *str)
 {
 int n, i, len;
-for (len = 0; str[len] !='\0'; len++ )
+for (len = 0; str[len] != '\0'; len++ )
 {}
-n= (len - 1)/2;
-for (i = n+1; str[i] != '\0'; i++)
+n = (len - 1) / 2;
+for (i = n + 1; str[i] != '\0'; i++)
 {
 putchar(str[i]);
 }
