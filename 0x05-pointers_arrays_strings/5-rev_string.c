213db78191ebefc1ac0778f8f961e454b4613690
yamen237 <joelyameni@gmail.com>
2022-03-27 00:34:09 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+/**
+ * rev_string - reverse a string 
+ * @s: take in character by pointer
+ */
+void rev_string(char *s)
+{
+char c;
+char *a;
+int i,n;
+n = strlen(s);
+for (i = 0; i < strlen(s); i++)
+{
+n--;
+c = *(s + n);
+*(a + i) = c;
+}
+s = a;
+}
